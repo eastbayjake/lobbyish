79f57264a057421054891c71b226fb3ad747f821
Jake McGuire <mcguireghs@gmail.com>
2015-01-04 22:53:15 -0800

    Require routes and models

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
 var express = require('express');
 var app = express();
+var requireDir = function(dir){
+	var path = require('path');
+	var fs = require('fs');
+	var normalizedPath = path.join(__dirname, dir);
+	fs.readdirSync(normalizedPath).forEach(function(file) {
+	  require(normalizedPath + '/' + file);
+	});
+};
+
+requireDir('routes');
+requireDir('models');
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-var server = app.listen(3000, function () {
+var server = app.listen(3000, function(){
 
   var host = server.address().address;
   var port = server.address().port;
